2040033ea5f12bae37b18176c180a51f85b6a956
lukinovec <lukinovec@gmail.com>
2021-04-05 22:42:03 +0200

    Welcome wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 @extends('template')
 @section('content')
 <div class="flex flex-col items-center justify-center w-full h-full">
-    <section class="p-4 m-4">
-        <h1 class="text-2xl font-bold">Welcome! <br></h1>
+    <section class="flex flex-col items-center justify-center p-4 m-4">
+        <div class="m-6">
+            <h1 class="text-2xl font-bold">Welcome to Porty! <br></h1>
+            <p>Porty makes a portfolio using your GitHub projects</p>
+        </div>
         @if ($user)
         <a href="/portfolio">
             View portfolio of {{ $user->nickname }}
         </a>
         @else
-        To generate a portfolio, <a href="/auth/redirect">log in with your GitHub account</a>.
+        <a href="/auth/redirect" class="p-2 text-xl font-semibold transition duration-500 ease-in-out delay-75 transform border-2 border-black hover:scale-125 rounded-xl">Generate a portfolio</a>
         @endif
     </section>
 </div>
